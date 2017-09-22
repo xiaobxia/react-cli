d5141bce276cb75b288d17cdcfb2f6273c960a31
chenlingjie <chenlingjie@cd121.com>
2017-09-22 15:43:34 +0800

    父子组件通讯

--- src/component/helloWorld/index.js
+++ src/component/helloWorld/index.js
@@ -13,24 +13,36 @@ class HelloWord extends Component {
   };
 
   state = {
-    time: new Date()
+    time: new Date(),
+    count: 0
   };
+
+  addCount = () => {
+
+    this.setState((preState) => {
+      this.props.onChange(preState.count + 1);
+      return {
+        count: preState.count + 1
+      }
+    });
+  };
+
 
   render() {
     //局部的类似于vue中的data
-    let {time} = this.state;
+    let {time, count} = this.state;
     //父组件传递给它的
-    let {address} = this.props;
+    let {name} = this.props;
     //console.log(this.props)
     return (
       <div>
         <h3>时间{time.toDateString()}</h3>
-        <p>地址{address}</p>
+        <p>计数{count}</p>
+        <p>名字{name}</p>
       </div>
     );
   }
 }
-
 export default HelloWord;
 
 //TODO 有了connect，就可以省去dispatch，
--- src/module/dashboard/index.js
+++ src/module/dashboard/index.js
@@ -6,6 +6,8 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 
+import HelloWord from 'localComponent/helloWorld'
+
 class Dashboard extends Component {
   //这种写法不用bind
   jumpToUser = () => {
@@ -47,12 +49,22 @@ class Dashboard extends Component {
     console.log(this.refs.input1.value)
   };
 
+  changeChildrenData = () => {
+    this.refs.helloword.addCount();
+  };
+
+  childrenChange = (newValue) => {
+    console.log(newValue)
+  };
+
   render() {
     //query在search里
     //console.log(this.props.location.search);
     return (
       <div>
         <h3>dashboard</h3>
+        <button onClick={this.changeChildrenData}>改变子组件</button>
+        <HelloWord ref="helloword" name="xiaobxia" onChange={this.childrenChange}></HelloWord>
         <p>input1: {this.state.input1Value}</p>
         <input ref="input1" value={this.state.input1Value} onChange={this.input1ValueChange}></input>
         <button onClick={this.jumpToUser}>去user</button>
