75af146659695ae6b07e3fc46f9354cbac30c149
chenlingjie <chenlingjie@cd121.com>
2017-10-23 13:28:56 +0800

    修改

--- src/module/main/header.js
+++ src/module/main/header.js
@@ -15,6 +15,28 @@ class AppHeader extends Component {
     });
   };
 
+  shouldComponentUpdate(nextProps, nextState) {
+    const thisProps = this.props || {};
+    const thisState = this.state || {};
+    nextState = nextState || {};
+    nextProps = nextProps || {};
+    if (Object.keys(thisProps).length !== Object.keys(nextProps).length ||
+      Object.keys(thisState).length !== Object.keys(nextState).length) {
+      return true;
+    }
+    for (const key in nextProps) {
+      if (thisProps[key] !== nextProps[key]) {
+        return true;
+      }
+    }
+    for (const key in nextState) {
+      if (thisState[key] !== nextState[key]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   render() {
     console.log('%cHeader render', 'background: #222; color: #bada55');
     let locale = this.props.intl.formatMessage;
