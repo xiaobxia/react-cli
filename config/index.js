a7433cfa280fa16f5f92b24a781f6905322e8b1b
chenlingjie <chenlingjie@cd121.com>
2017-12-18 09:50:12 +0800

    xg

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report,
-    idCdn: false
+    ifCdn: false
   },
   dev: {
     env: require('./dev.env'),
